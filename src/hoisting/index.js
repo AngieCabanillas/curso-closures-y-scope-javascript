585ff2534f18c211d8f7a16d570be29f3333a78e
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-08-16 14:12:27 -0500

    chore: :white_check_mark: Debugging

--- src/hoisting/index.js
+++ src/hoisting/index.js
@@ -3,3 +3,17 @@ function nameOfDog(){
     console.log("el mejor perrito es: " + elmo);
 }
 var elmo; 
+
+//
+
+var a = "Hello global"
+
+function hello() {
+  let b = "Hello function"
+
+  if(true) {
+    let c = "Hello block"
+  }
+}
+
+hello()
